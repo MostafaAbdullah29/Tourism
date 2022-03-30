9f56166d9cac84f3e81813a9f03028f71266abda
Mostafaaq <102669535+Mostafaaq@users.noreply.github.com>
2022-03-30 14:37:07 +0200

    upload website files

---
+++ script.js
@@ -0,0 +1,12 @@
+ 
+// using JavaScript to add two messages for the buttons :
+function cancel_msg() {
+  alert("Sorry! Your request has been cancelled.");
+}
+
+function submit_msg() {
+  alert("Thank you! Submission Successful.");
+}
+
+
+
